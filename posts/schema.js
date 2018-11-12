8ea2d41632ee3ab8ce2117bca098318cc086f782
nicholasgriffen <nicholas.s.griffen@gmail.com>
2018-11-11 17:34:19 -0700

    validate uuid

--- posts/schema.js
+++ posts/schema.js
@@ -1,7 +1,7 @@
 const Joi = require('joi')
 module.exports = {
     content: Joi.object().keys({
-        content: Joi.string().required()
+        content: Joi.string().guid().required()
     }),
     ID: Joi.object().keys({
         id: Joi.string().alphanum().required(),
